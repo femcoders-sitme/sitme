c8d531153f474bde97dcd9923a60bda1857e7de4
Débora Rubio <debora.rubio.soliva@gmail.com>
2025-09-10 12:28:06 +0200

    first commit

---
+++ src/main/java/com/femcoders/sitme/SitmeApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.sitme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/sitme/SitmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.sitme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
