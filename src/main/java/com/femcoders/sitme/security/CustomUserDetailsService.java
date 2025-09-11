313c142a821f46dff3fb9d471c28b6d80f3678fe
Lara Pla <laraplamoreno@gmail.com>
2025-09-11 13:19:28 +0200

    feat(CustomUserDetailsService): add loadUserByUsername method

---
+++ src/main/java/com/femcoders/sitme/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.femcoders.sitme.security;
+
+import com.femcoders.sitme.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByUsername(username);
+
+        return user.map(userEntity -> new CustomUserDetails(userEntity))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
