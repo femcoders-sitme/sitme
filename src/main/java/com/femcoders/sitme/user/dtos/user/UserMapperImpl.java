6c4042e7189be966778a048fe6926c683fa73995
saba <sabaurrehman11@gmail.com>
2025-09-13 00:51:42 +0200

    feat: implement UserMapper with methods to convert between User and DTs

--- src/main/java/com/femcoders/sitme/user/dtos/user/UserMapperImpl.java
+++ src/main/java/com/femcoders/sitme/user/dtos/user/UserMapperImpl.java
@@ -1,4 +1,30 @@
 package com.femcoders.sitme.user.dtos.user;
 
-public class UserMapperImpl {
+import com.femcoders.sitme.user.Role;
+import com.femcoders.sitme.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User dtoToEntity(UserRequest userRequestDTO, Role role) {
+        return User.builder()
+                .username(userRequestDTO.username())
+                .email(userRequestDTO.email())
+                .password(userRequestDTO.password())
+                .role(role)
+                .build();
+    }
+
+    @Override
+    public UserResponse entityToDto(User user) {
+        return new UserResponse(
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole(),
+                user.getCreatedAt(),
+                user.getReservations()
+        );
+    }
 }
