5dee526444102794e7c9a3e048e101041c28dab3
saba <sabaurrehman11@gmail.com>
2025-09-13 00:54:01 +0200

    feat: add UserService interface with getUserById method

--- src/main/java/com/femcoders/sitme/user/services/UserService.java
+++ src/main/java/com/femcoders/sitme/user/services/UserService.java
@@ -1,4 +1,8 @@
 package com.femcoders.sitme.user.services;
 
+import com.femcoders.sitme.user.dtos.user.UserResponse;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface UserService {
+    UserResponse getUserById(Long id, UserDetails userDetails);
 }
