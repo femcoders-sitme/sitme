5205eada2fb4755df46ca3226cb37e849b351cd3
saba <sabaurrehman11@gmail.com>
2025-09-13 00:54:58 +0200

    feat: add UserController with endpoint to get user by id

--- src/main/java/com/femcoders/sitme/user/controller/UserController.java
+++ src/main/java/com/femcoders/sitme/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.femcoders.sitme.user.controller;
 
+import com.femcoders.sitme.user.dtos.user.UserResponse;
+import com.femcoders.sitme.user.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(
+            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        UserResponse userResponse = userService.getUserById(id, userDetails);
+        return ResponseEntity.ok(userResponse);
+    }
 }
