2144fe5b18da1f45c8cba23e1486d6a4d6470919
saba <sabaurrehman11@gmail.com>
2025-09-14 13:44:55 +0200

    feat: implement UserServiceImpl with repository and mapper for getUserById

--- src/main/java/com/femcoders/sitme/user/services/UserServiceImpl.java
+++ src/main/java/com/femcoders/sitme/user/services/UserServiceImpl.java
@@ -1,4 +1,29 @@
 package com.femcoders.sitme.user.services;
 
-public class UserServiceImpl {
+import com.femcoders.sitme.user.User;
+import com.femcoders.sitme.user.dtos.user.UserMapper;
+import com.femcoders.sitme.user.dtos.user.UserResponse;
+import com.femcoders.sitme.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserResponse getUserById(Long id, UserDetails userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id " + id));
+        return userMapper.entityToDto(user);
+    }
+
 }
