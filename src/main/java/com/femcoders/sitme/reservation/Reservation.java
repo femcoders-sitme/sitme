11b27957f321a4f1216f8afda700a9f57752ed79
saba <sabaurrehman11@gmail.com>
2025-09-14 13:43:08 +0200

    feat: add ManyToOne relations to User and Space in Reservation entity

--- src/main/java/com/femcoders/sitme/reservation/Reservation.java
+++ src/main/java/com/femcoders/sitme/reservation/Reservation.java
@@ -1,5 +1,7 @@
 package com.femcoders.sitme.reservation;
 
+import com.femcoders.sitme.space.Space;
+import com.femcoders.sitme.user.User;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,4 +10,12 @@ public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "space_id")
+    private Space space;
 }
