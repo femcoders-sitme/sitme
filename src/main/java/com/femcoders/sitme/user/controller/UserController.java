9c447168ddc1cfefeff54aa6f81a1b4e34eb41b9
saba <sabaurrehman11@gmail.com>
2025-09-15 14:24:45 +0200

    refactor(UserController): add loombok anotation for constructor

--- src/main/java/com/femcoders/sitme/user/controller/UserController.java
+++ src/main/java/com/femcoders/sitme/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.femcoders.sitme.user.controller;
 
 import com.femcoders.sitme.user.dtos.user.UserResponse;
 import com.femcoders.sitme.user.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,14 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(
