4a6b55ee19dcb245a42a1c0c13ed39e25a47bd22
Lara Pla <laraplamoreno@gmail.com>
2025-09-16 18:34:36 +0200

    feat(ReservationRequest/Response/Mapper): add reservation dtos

---
+++ src/main/java/com/femcoders/sitme/reservation/dtos/ReservationMapper.java
@@ -0,0 +1,35 @@
+package com.femcoders.sitme.reservation.dtos;
+
+import com.femcoders.sitme.reservation.Reservation;
+import com.femcoders.sitme.reservation.Status;
+import com.femcoders.sitme.reservation.TimeSlot;
+import com.femcoders.sitme.space.Space;
+import com.femcoders.sitme.user.User;
+
+public class ReservationMapper {
+
+    public static Reservation dtoToEntity(ReservationRequest request, User user, Space space) {
+        return Reservation.builder()
+                .reservationDate(request.reservationDate())
+                .timeSlot(request.timeSlot())
+                .status(Status.ACTIVE)
+                .emailSent(false)
+                .user(user)
+                .space(space)
+                .build();
+    }
+
+    public static ReservationResponse entityToDto(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.getReservationDate(),
+                reservation.getTimeSlot(),
+                reservation.getStatus(),
+                reservation.isEmailSent(),
+                reservation.getCreatedAt(),
+                reservation.getUser().getId(),
+                reservation.getUser().getUsername(),
+                reservation.getSpace().getId(),
+                reservation.getSpace().getName()
+        );
+    }
+}
